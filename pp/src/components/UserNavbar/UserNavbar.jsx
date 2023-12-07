cbf96577a34534f0d3595450c9bbe52b5d87a7a2
heeyyeon <wjdgmldus28@gachon.ac.kr>
2023-12-07 11:24:58 +0900

    유저 이미지가 블랙일 경우 nav

---
+++ pp/src/components/UserNavbar/UserNavbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './UserNavbar.css';
+import dataIcon from '../../images/icons/data-gray.svg'; 
+import pointIcon from '../../images/icons/point-gray.svg'; 
+import userIcon from '../../images/icons/user-black.svg'; 
+
+function NavItem({ icon, label, className }) {
+  return (
+    <div className={`nav-item ${className}`}>
+      {icon && <img src={icon} alt={label} className="nav-icon" />}
+      <span>{label}</span>
+    </div>
+  );
+}
+
+function UserNavbar() {
+  return (
+    <div className="nav-bar">
+      <NavItem icon={dataIcon} label="데이터 수집" className="text-gray" />
+      <NavItem icon={pointIcon} label="잔액" className="text-gray" />
+      <NavItem icon={userIcon} label="사용자" />
+    </div>
+  );
+}
+
+export default UserNavbar;
