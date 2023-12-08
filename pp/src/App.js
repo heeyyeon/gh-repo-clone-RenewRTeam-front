66e124c8af961981c9846f69077d31aaf5bff00e
heeyyeon <wjdgmldus28@gachon.ac.kr>
2023-12-09 08:45:01 +0900

    [12/8] 라우터 경로 수정

--- pp/src/App.js
+++ pp/src/App.js
@@ -24,64 +24,10 @@
 
 // // export default App;
 
-// import React from 'react';
-// import Card from './components/Card/Card';
-// //import Navbar from './components/Navbar/Navbar';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="app">
-//       {/* Card 컴포넌트에 데이터를 전달 */}
-//       <Card 
-//         image="path_to_image.jpg" 
-//         title="카드 제목" 
-//         description="여기에 카드 설명을 적습니다."
-//         limit="정원"
-//       />
-//       {/* <Navbar /> */}
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // 'Routes' 추가
-// import CardList from './pages/CardList/CardList';
-// import Detail from './pages/Detail/Detail';
-// import './App.css';
-
-// function App() {
-//   const cards = [
-//     { image: 'https://images.unsplash.com/photo-1596854372407-baba7fef6e51?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', title: 'Title 1', description: 'Description 1', limit: 'limit 1' },
-//     { image: 'image2.jpg', title: 'Title 2', description: 'Description 2', limit: 'limit 2' },
-//     { image: 'image2.jpg', title: 'Title 2', description: 'Description 2', limit: 'limit 2' },
-//     { image: 'image2.jpg', title: 'Title 2', description: 'Description 2', limit: 'limit 2' },
-//   ];
-
-//   return (
-//     <Router>
-//       <div className="app">
-//         <Routes> {/* 'Routes' 컴포넌트 사용 */}
-//           <Route path="/" element={<CardList cards={cards} listTitle="데이터 거래 목록" />} />
-//           <Route path="/detail/:id" element={<Detail />} />
-//           {/* ... 추가 라우트 ... */}
-//         </Routes>
-//         {/* BottomBar 컴포넌트 활성화하려면 주석 해제 */}
-//         {/* <BottomBar /> */}
-//       </div>
-//     </Router>
-//   );
-// }
-// export default App;
-
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CardList from './pages/CardList/CardList';
 import Detail from './pages/Detail/Detail';
-// import BottomBar from './components/BottomBar/BottomBar'; // 필요하다면 주석 해제
 import './App.css';
 
 function App() {
@@ -89,12 +35,13 @@ function App() {
     <Router>
       <div className="app">
         <Routes>
-        <Route path="/" element={<CardList listTitle="데이터 수집 목록" />} />
+          <Route path="/" element={<CardList listTitle="데이터 수집 목록" />} />
           <Route path="/detail/:id" element={<Detail />} />
           {/* 기타 라우트는 필요에 따라 추가 */}
         </Routes>
       </div>
     </Router>
+
   );
 }
 
--- pp/src/index.js
+++ pp/src/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+// import React from 'react';
+// // import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import CardList from "./pages/CardList/CardList";
+
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
@@ -12,16 +15,36 @@ import './index.css';
 //   </React.StrictMode>
 // );
 
-import Register from "./pages/Register/Register";
-//중요한 코드!
- ReactDOM.render(
- <React.StrictMode>
- <Register />
- </React.StrictMode>,
- document.getElementById('root')
-);
+// import CardList from "./pages/CardList/CardList";
+//  ReactDOM.render(
+//  <React.StrictMode>
+//  <CardList />
+//  </React.StrictMode>,
+//  document.getElementById('root')
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <CardList />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+// import App from './App'; // App 컴포넌트 임포트 (배포할 때 주석 풀기)
+import Delete from './pages/Delete/Delete';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Delete /> 
+  </React.StrictMode>
+);
