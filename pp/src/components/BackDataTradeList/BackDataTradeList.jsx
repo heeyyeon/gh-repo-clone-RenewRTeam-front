d0aa135bb46112eec0ae58e722a4fc13d1974ade
heeyyeon <wjdgmldus28@gachon.ac.kr>
2023-12-13 05:01:53 +0900

    [12/12]@ 뒤로 가기 아이콘 전 링크 넘어가도록 코드 추가

--- pp/src/components/BackDataTradeList/BackDataTradeList.jsx
+++ pp/src/components/BackDataTradeList/BackDataTradeList.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import back from '../../images/back.svg';
 import './BackDataTradeList.css'; 
 
 function BackDataTradeList({ listTitle }) {
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    navigate(-1); // 브라우저 히스토리에서 한 단계 뒤로 이동
+  };
+
   return (
     <div className="data-trade-list-container">
-      <img src={back} alt="back Logo" />
+      <img src={back} alt="back Logo" onClick={handleBackClick} />
       <h3 className="data-trade-list-title">{listTitle}</h3>
     </div>
   );
