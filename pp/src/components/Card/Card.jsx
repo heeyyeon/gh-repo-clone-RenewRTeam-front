f3709671af7f108c4c51a27012df791b4d4088f6
heeyyeon <wjdgmldus28@gachon.ac.kr>
2023-12-12 09:23:59 +0900

    불필요한 버튼 컴포넌트 삭제

--- pp/src/components/Card/Card.jsx
+++ pp/src/components/Card/Card.jsx
@@ -1,16 +1,47 @@
+// import React from 'react';
+// import './Card.css';
+
+// function Card({ image, title, description, limit }) {
+//   return (
+//       <div className="card">
+//         <img src={image} alt="content" className="card-image" />
+//         <div className="card-details">
+//           <h2 className="card-title">{title}</h2>
+//           <p className="card-description">{description}</p>
+//           <p className="card-limit">{limit}</p>
+//         </div>
+//       </div>
+//   );
+// }
+
+// export default Card;
+
 import React from 'react';
 import './Card.css';
 
-function Card({ image, title, description, limit }) {
+function Card({ image, title, description, tag }) {
+
+  let tagClassName = '';
+
+  if (tag === '참여중') {
+    tagClassName = 'chamyeojung';
+  } else if (tag === '거절') {
+    tagClassName = 'geojul';
+  } else if (tag === '승인') {
+    tagClassName = 'seungin';
+  } else if (tag === '대기') {
+    tagClassName = 'daegi';
+  }
+
   return (
-      <div className="card">
-        <img src={image} alt="content" className="card-image" />
-        <div className="card-details">
-          <h2 className="card-title">{title}</h2>
-          <p className="card-description">{description}</p>
-          <p className="card-limit">{limit}</p>
-        </div>
+    <div className="card">
+      <img src={image} alt="content" className="card-image" />
+      <div className="card-details">
+        <h2 className="card-title">{title}</h2>
+        <p className="card-description">{description}</p>
+        {tag && <span className={`card-tag ${tagClassName}`}>{tag}</span>}
       </div>
+    </div>
   );
 }
 
