02e8453a43c4dab64d80824eab80fd7ad331fbf5
heeyyeon <wjdgmldus28@gachon.ac.kr>
2023-12-14 21:56:32 +0900

    [12/14] 제공자 리워드 페이지 api 연결 / 내역 이미지 아이콘 추가

--- pp/src/pages/Reward/Reward.jsx
+++ pp/src/pages/Reward/Reward.jsx
@@ -1,68 +1,92 @@
-import React, { useState } from 'react';
-import Trading from '../../components/Trading/Trading';
-import { useNavigate } from 'react-router-dom';
-import './Reward.css';
-import DataTradeList from '../../components/DataTradeList/DataTradeList';
-import RewardNavbar from '../../components/RewardNavbar/RewardNavbar';
+import React, { useState, useEffect } from "react";
+import Trading from "../../components/Trading/Trading";
+import { useNavigate } from "react-router-dom";
+import "./Reward.css";
+import DataTradeList from "../../components/DataTradeList/DataTradeList";
+import RewardNavbar from "../../components/RewardNavbar/RewardNavbar";
+import transaction_icon from "../../images/transaction_icon.svg";
 
 function Reward() {
-    const navigate = useNavigate();
-    const [model] = useState({
-        myReward: '본인이 가진 리워드',
-        image: 'https://via.placeholder.com/150',
-        name: '거래한 사람이름',
-        date: '거리한 날짜',
-        trading: '입출금 내역',
-        balance: '잔액'
-    });
+  const navigate = useNavigate();
+  // const [model] = useState({
+  //     myReward: '본인이 가진 리워드',
+  //     image: 'https://via.placeholder.com/150',
+  //     name: '거래한 사람이름',
+  //     date: '거리한 날짜',
+  //     trading: '입출금 내역',
+  //     balance: '잔액'
+  // });
 
-    const handleTransferClick = () => {
-        navigate('/RewardSend'); // RewardSend 컴포넌트로 이동
+  const handleTransferClick = () => {
+    navigate("/RewardSend"); // RewardSend 컴포넌트로 이동
+  };
+  const [models, setModels] = useState([]);
+  const endpoint = "/reward/history";
+
+  useEffect(() => {
+    const fetchRewardHistory = async () => {
+      try {
+        const response = await fetch(endpoint, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + window.sessionStorage.getItem("token"),
+          },
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setModels(data.data);
+
+          console.log("성공:", data);
+        } else {
+          const errorText = await response.text();
+          alert(`실패: ${errorText}`);
+        }
+      } catch (error) {
+        alert("오류가 발생했습니다.");
+        console.error("API 호출 중 오류 발생:", error);
+      }
     };
 
-    return (
-        <>
-        <DataTradeList listTitle={"리워드"} />
-        <div className='reward'>
-            <p style={{ 
-                color: 'black', margin:'30px 0px 0px 16px', fontSize:'16px'}}>당신의 리워드는</p>
-            <p style={{margin:'6px 16px', fontSize:'32px'}}><b>{model ? model.myReward : "Loading..."} </b></p>
-            <p style={{margin:'6px 16px', fontSize:'16px'}}>입니다.</p>
-            {/** 버튼 컴포넌트 들어올 자리, 지금은 만든게 없어서 대체 */}
-            <div className='reward-button' onClick={handleTransferClick}><b>이체하기</b></div>
-            <p style={{color:'#A5A5A5', marginLeft:'16px'}}>거래내역</p>
-            <Trading
-            image src={model ? model.image : "defaul_image.png"} 
-            name= {model ? model.name : "Loading..."}
-            date= {model ? model.date : "Loading..."}
-            trading= {model ? model.trading : "Loading..."}
-            balance= {model ? model.balance : "Loading..."}
-            />
-            <Trading
-            image src={model ? model.image : "defaul_image.png"} 
-            name= {model ? model.name : "Loading..."}
-            date= {model ? model.date : "Loading..."}
-            trading= {model ? model.trading : "Loading..."}
-            balance= {model ? model.balance : "Loading..."}
-            />
-            <Trading
-            image src={model ? model.image : "defaul_image.png"} 
-            name= {model ? model.name : "Loading..."}
-            date= {model ? model.date : "Loading..."}
-            trading= {model ? model.trading : "Loading..."}
-            balance= {model ? model.balance : "Loading..."}
-            />
-            <Trading
-            image src={model ? model.image : "defaul_image.png"} 
-            name= {model ? model.name : "Loading..."}
-            date= {model ? model.date : "Loading..."}
-            trading= {model ? model.trading : "Loading..."}
-            balance= {model ? model.balance : "Loading..."}
-            />
+    fetchRewardHistory();
+  }, []);
+
+  return (
+    <>
+      <DataTradeList listTitle={"리워드"} />
+      <div className="reward">
+        <p
+          style={{
+            color: "black",
+            margin: "30px 0px 0px 16px",
+            fontSize: "16px",
+          }}
+        >
+          당신의 리워드는
+        </p>
+        <p style={{ margin: "6px 16px", fontSize: "32px" }}>
+          <b>{models ? models.myReward : "Loading..."} </b>
+        </p>
+        <p style={{ margin: "6px 16px", fontSize: "16px" }}>입니다.</p>
+        {/** 버튼 컴포넌트 들어올 자리, 지금은 만든게 없어서 대체 */}
+        <div className="reward-button" onClick={handleTransferClick}>
+          <b>이체하기</b>
         </div>
-        <RewardNavbar />
-        </>
-    )
+        <p style={{ color: "#A5A5A5", marginLeft: "16px" }}>거래내역</p>
+        {models.map((model, index) => (
+          <Trading
+            image={transaction_icon}
+            name={model ? model.sender : "Loading..."}
+            date={model ? model.createdAt : "Loading..."}
+            trading={model ? model.amount : "Loading..."}
+            balance={model ? model.total : "Loading..."}
+          />
+        ))}
+      </div>
+      <RewardNavbar />
+    </>
+  );
 }
 
 export default Reward;
