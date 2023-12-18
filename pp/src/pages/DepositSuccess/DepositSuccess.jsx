9c1a369c93b7969f72b6ad90a26b8d73dcb9eda7
heeyyeon <wjdgmldus28@gachon.ac.kr>
2023-12-18 17:44:18 +0900

    [12/18] 입금 성공 페이지 라우터 연결

--- pp/src/pages/DepositSuccess/DepositSuccess.jsx
+++ pp/src/pages/DepositSuccess/DepositSuccess.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 import './DepositSuccess.css';
 import BackDataTradeList from '../../components/BackDataTradeList/BackDataTradeList';
 import PointNavbar from '../../components/PointNavbar/PointNavbar';
+import { useNavigate } from 'react-router-dom';
 
 function DepositSuccess() {
+
+
+    const navigate = useNavigate();
+
+    const handleOKButton = () => {
+        navigate('/Reward2');
+    }
     return (
         <>
         <BackDataTradeList listTitle={"입금"} />
@@ -13,7 +21,7 @@ function DepositSuccess() {
                 <p>안전하게</p>
                 <p>입금되었습니다.</p>
             </div>
-            <div className='checkButton'>확인</div>
+            <div className='checkButton' onClick={handleOKButton}>확인</div>
         </div>
         <PointNavbar />
         </>
